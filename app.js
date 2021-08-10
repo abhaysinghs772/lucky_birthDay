c7562641139657272466e52c90fe7c4e958ebe6f
abhaysinghs772 <abhaysinghs772@gmail.com>
2021-08-10 23:49:20 +0530

    styles- completed styles part and a bit logic

--- app.js
+++ app.js
@@ -3,9 +3,45 @@
 const birthDate = document.querySelector('.birthDate');
 const luckyNumber = document.querySelector('.luckyNumberInput');
 const checkButton = document.querySelector('#checkBtn');
+const luckyMessage = document.querySelector('.luckyMsz');
+const unLuckyMessage = document.querySelector('.unLuckyMsz');
+const privacyMessage = document.querySelector('.privacyMsz');
+const privacyToggler= document.querySelector('.privacyToggler');
 
-checkButton.addEventListener('click', ()=>{
-    console.log('clicked');
-    console.log(birthDate.value);
-    console.log(luckyNumber.value);
+let birth_Date_To_NUmber;
+let luckyNumber_To_Number;
+let onlyStrings_Of_birthDate;
+
+privacyToggler.addEventListener('click', ()=>{
+    hideMsz(privacyMessage);
 });
+
+checkButton.addEventListener('click', () => {
+    console.log('clicked');
+    // console.log(birthDate.value);
+    // console.log(luckyNumber.value);
+
+    onlyStrings_Of_birthDate = birthDate.value.replaceAll('-', '');
+
+    birth_Date_To_NUmber = Number(onlyStrings_Of_birthDate);
+    console.log(birth_Date_To_NUmber);
+
+    luckyNumber_To_Number = Number(luckyNumber.value);
+    console.log(luckyNumber_To_Number);
+
+    if (birth_Date_To_NUmber % luckyNumber_To_Number === 0) {
+        console.log('you are lucky man');
+        showMsz(luckyMessage);
+    } else {
+        console.log('hey buddy! you are too lucky man!');
+        showMsz(unLuckyMessage);
+    }
+});
+
+function showMsz(xyz) {
+    return xyz.style.display = 'block';
+}
+
+function hideMsz(xyz) {
+    return xyz.style.display = 'none';
+}
